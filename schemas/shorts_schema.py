cea5ab7c041f6f0e3460ea96ca8d28d5a1b89187
An, Seong Hyeon <61452538+An-Seonghyeon@users.noreply.github.com>
2025-08-18 11:24:52 +0000

    fix: VideoResponse 스키마 AWS s3 key 지정 & 불필요한 data 제거

--- schemas/shorts_schema.py
+++ schemas/shorts_schema.py
@@ -2,9 +2,11 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+
 class Scenario(BaseModel):
     title: str
     content: str
+
 
 class ScenarioRequest(BaseModel):
     # 매장 정보
@@ -22,13 +24,16 @@ class ScenarioRequest(BaseModel):
     # 사용자의 시나리오 요구사항
     scenario_prompt: str
 
+
 class ScenarioResponse(BaseModel):
     session_id: str
     scenarios: List[Scenario] = Field(default_factory = list)
 
+
 class Scene(BaseModel):
     title: str
     content: str
+
 
 class VideoRequest(BaseModel):
     session_id: str
@@ -37,19 +42,10 @@ class VideoRequest(BaseModel):
     ad_duration: int
     image_list: List[str]
 
+
 class VideoResponse(BaseModel):
-    # video_url: str
+    key: Optional[str] = Field(default = None, description = "S3에 저장된 최종 비디오 key")
 
-    # TODO: 테스트
-    scenes: List[Scene]
-    ai_scenes_image_list: List[str]
-
-    final_video_path: Optional[str] = Field(default = None, description = "최종 비디오 파일 경로")
-    final_video_filename: Optional[str] = Field(default = None, description = "최종 비디오 파일명")
-    
-    # 개별 씬 비디오 URL
-    scene_video_urls: List[str] = Field(default_factory = list, description = "각 씬별 생성된 비디오 URL")
-    
 
 class InputImageInfo(BaseModel):
     url: str
