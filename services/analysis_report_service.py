43ed95859bc1d0b87e8d4d0e72ff7980742c5691
An, Seong Hyeon <61452538+An-Seonghyeon@users.noreply.github.com>
2025-08-26 09:41:39 +0000

    feat: 성과분석 보고서 생성 서비스 생성

---
+++ services/analysis_report_service.py
@@ -0,0 +1,15 @@
+from typing import Dict, Any
+from nodes.comments_analyzer.analysis_report import AnalysisReport
+from nodes.comments_analyzer.generate_korean_md_report import generate_korean_markdown_report
+from schemas.report_analysis_schema import PostAnalysisRequest, PostAnalysisResponse, PostAnalysisMarkdownReport
+
+
+def generate_final_report(request: PostAnalysisRequest) -> PostAnalysisMarkdownReport:
+    service = AnalysisReport()
+    
+    report_data = service.generate_analysis_report(request)
+    analysis_response = PostAnalysisResponse(**report_data)
+
+    markdown_report = generate_korean_markdown_report(analysis_response, request)
+    
+    return PostAnalysisMarkdownReport(markdown_report = markdown_report)
