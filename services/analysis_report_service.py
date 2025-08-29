b35cabc246647bff4733f1cefbd4a4d109b7c9c0
An, Seong Hyeon <61452538+An-Seonghyeon@users.noreply.github.com>
2025-08-29 08:36:01 +0000

    feat: 업종 정보 파라미터 및 로그 추가

--- services/analysis_report_service.py
+++ services/analysis_report_service.py
@@ -6,10 +6,13 @@ from schemas.report_analysis_schema import PostAnalysisRequest, PostAnalysisResp
 
 def generate_final_report(request: PostAnalysisRequest) -> PostAnalysisMarkdownReport:
     service = AnalysisReport()
+          
+    report_data, classified_industry = service.generate_analysis_report(request)
+
+    print(f"보고서 생성 입력 데이터: {report_data}")
     
-    report_data = service.generate_analysis_report(request)
     analysis_response = PostAnalysisResponse(**report_data)
 
-    markdown_report = generate_korean_markdown_report(analysis_response.dict(), request)
+    markdown_report = generate_korean_markdown_report(analysis_response.dict(), classified_industry, request)
     
     return PostAnalysisMarkdownReport(markdown_report = markdown_report)
