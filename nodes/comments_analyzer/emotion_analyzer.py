0e7c682085bd832845510fdad1f33a8190f80480
An, Seong Hyeon <61452538+An-Seonghyeon@users.noreply.github.com>
2025-09-01 10:25:26 +0000

    fix: 한국어 댓글 감정 분석 및 키워드 추출 모델 버전 변경

--- nodes/comments_analyzer/emotion_analyzer.py
+++ nodes/comments_analyzer/emotion_analyzer.py
@@ -5,11 +5,13 @@ from typing import List, Dict, Any
 # 출처: https://huggingface.co/Copycats/koelectra-base-v3-generalized-sentiment-analysis
 # 출처: https://huggingface.co/jhgan/ko-sroberta-multitask
 
+# Replicate 모델 주소: https://replicate.com/choiminji-020102/ko-comments-emotion-analyzer
 def analyze_emotions_batch(texts: List[str], ids: List[int] = None) -> Dict[str, Any]:
     replicate_client = replicate.Client(api_token = settings.replicate_api_key)
 
     output = replicate_client.run(
-        "choiminji-020102/ko-comments-emotion-analyzer:03ee810894a2120c443e65b7ec822dedbd72adcc40cc5109ddfee65de42da387",
+        "choiminji-020102/ko-comments-emotion-analyzer:55f49ebbdc50dd9565906847fb96d0621a19e1122536df254d995d61437b0d1f",
+        
         input = {
             "texts": texts,
             "ids": ids
